9d507028bdb7c44d1f1bcdf761d88b7da25287dd
shivam675 <shivam31199@gmail.com>
2021-07-14 22:01:54 +0530

    added pcd file saver

---
+++ pointcloud_saver/scripts/point_cloud_saver.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import rospy
+import sensor_msgs.point_cloud2 as pc2
+import pcl
+# import tf
+
+
+
+def ros_to_pcl(ros_cloud):
+    points_list = []
+    for data in pc2.read_points(ros_cloud, skip_nans=True):
+        points_list.append([data[0], data[1], data[2], data[3]])
+
+    pcl_data = pcl.PointCloud_PointXYZRGB()
+    pcl_data.from_list(points_list)
+
+    return pcl_data
+
+
+def point_cloud_callback(pcl_msg):
+    cloud = ros_to_pcl(pcl_msg)
+    vox = cloud.make_voxel_grid_filter()
+    LEAF_SIZE = 0.006
+    vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
+    cloud_voxel = vox.filter()
+    pcl.save(cloud_voxel, "cloud_name.pcd")
+
+
+
+if __name__ == "__main__":
+    rospy.init_node("point_cloud_sub", anonymous=True)
+    rospy.loginfo("INITIALIZED....")
+    # pc_sub = rospy.Subscriber("/camera_intelrealsense/depth/point_cloud", pc2.PointCloud2, point_cloud_callback, queue_size=3)
+    pc_sub_msg = rospy.wait_for_message("/points", pc2.PointCloud2, timeout=None)
+    point_cloud_callback(pc_sub_msg)
+    # while not rospy.is_shutdown():
+    #     rospy.spin()
